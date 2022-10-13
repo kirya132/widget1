4e6bc154d81f12fb095837ff0a988f466e8f5895
kirya132 <shkulov_kirill@mail.ru>
2022-10-13 15:40:19 +0600

    input

--- src/main/java/com/example/widget/HelloController.java
+++ src/main/java/com/example/widget/HelloController.java
@@ -1,14 +1,12 @@
 package com.example.widget;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private ImageView SteamUp;
+
 }
+
