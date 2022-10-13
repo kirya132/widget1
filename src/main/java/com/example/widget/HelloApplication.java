2f0070163a0bf4b74a599b0a522d774dd3e7c8be
kirya132 <shkulov_kirill@mail.ru>
2022-10-13 14:44:27 +0600

    input

--- src/main/java/com/example/widget/HelloApplication.java
+++ src/main/java/com/example/widget/HelloApplication.java
@@ -18,9 +18,7 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        System.out.println("fvdf");
-        System.out.println("fvdfv");
-        System.out.println("Hello");
+
         launch();
     }
 }
