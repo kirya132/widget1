81f11c64f7c60702591a19c881b5ace4e3b64694
kirya132 <shkulov_kirill@mail.ru>
2022-10-17 13:12:59 +0600

    input

--- src/main/java/com/example/widget/HelloController.java
+++ src/main/java/com/example/widget/HelloController.java
@@ -1,12 +1,60 @@
 package com.example.widget;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class HelloController {
 
     @FXML
-    private ImageView SteamUp;
+    private Button buttondiscord;
 
+    @FXML
+    private Button buttonsteam;
+
+    @FXML
+    private Button mycomputer;
+
+
+    public void mouseClickedSteam() throws IOException {
+        if (Desktop.isDesktopSupported()) {
+            Desktop desktop = Desktop.getDesktop();
+            if (desktop.isSupported(Desktop.Action.OPEN)) {
+                try {
+                    desktop.open(new File("C:\\Program Files (x86)\\Steam\\Steam.exe"));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    public void mouseClickedDiscord() {
+        if (Desktop.isDesktopSupported()) {
+            Desktop desktop = Desktop.getDesktop();
+            if (desktop.isSupported(Desktop.Action.OPEN)) {
+                try {
+                    desktop.open(new File("C:\\Users\\User\\AppData\\Local\\Discord\\app-1.0.9006\\Discord.exe"));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    public void mouseClickedComputer() throws IOException {
+        Runtime.getRuntime().exec("cmd /c start explorer");
+    }
+
+
+    public void mouseClickedClose() {
+        System.exit(1);
+    }
 }
-
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.widget {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.datatransfer;
+    requires java.desktop;
 
 
     opens com.example.widget to javafx.fxml;
