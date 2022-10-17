108d5d424e61e7019d0f6033bc979d935e0efb18
kirya132 <shkulov_kirill@mail.ru>
2022-10-17 15:32:09 +0600

    Effects

--- src/main/java/com/example/widget/HelloController.java
+++ src/main/java/com/example/widget/HelloController.java
@@ -4,6 +4,9 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.effect.Effect;
+import javafx.scene.effect.Glow;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -12,16 +15,23 @@ import java.io.File;
 import java.io.IOException;
 
 public class HelloController {
+    @FXML
+    private ImageView Computerimg;
 
     @FXML
-    private Button buttondiscord;
+    private ImageView Chromeimg;
 
     @FXML
-    private Button buttonsteam;
+    private ImageView Discordimg;
 
     @FXML
-    private Button mycomputer;
+    private ImageView Steamimg;
 
+    @FXML
+    private ImageView Yandeximg;
+
+    @FXML
+    private ImageView Discord;
 
     public void mouseClickedSteam() throws IOException {
         if (Desktop.isDesktopSupported()) {
@@ -56,5 +66,43 @@ public class HelloController {
 
     public void mouseClickedClose() {
         System.exit(1);
+    }
+
+    public void mouseClickedYandex() {
+        if (Desktop.isDesktopSupported()) {
+            Desktop desktop = Desktop.getDesktop();
+            if (desktop.isSupported(Desktop.Action.OPEN)) {
+                try {
+                    desktop.open(new File("C:\\Users\\User\\AppData\\Local\\Yandex\\YandexBrowser\\Application\\browser.exe"));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    public void EnteredSteam() {
+        Steamimg.setOpacity(1);
+        Steamimg.setFitHeight(53);
+        Steamimg.setFitWidth(55);
+    }
+
+
+    public void ExitedSteam() {
+        Steamimg.setOpacity(0.6);
+        Steamimg.setFitHeight(49);
+        Steamimg.setFitWidth(47);
+    }
+
+    public void EntDiscord() {
+        Discordimg.setOpacity(1);
+       Discordimg.setFitHeight(53);
+       Discordimg.setFitWidth(55);
+    }
+
+    public void ExDiscord() {
+        Discordimg.setOpacity(0.6);
+        Discordimg.setFitHeight(49);
+      Discordimg.setFitWidth(47);
     }
 }
